77c73d460700236919429146f4eb9db1bba9b116
cori-hwf <huangweifeng.cori@gmail.com>
2024-01-12 17:49:01 +0800

    setMeal service and serviceImp

--- src/main/java/com/food/delivery/Service/ServiceImp/SetmealServiceImp.java
+++ src/main/java/com/food/delivery/Service/ServiceImp/SetmealServiceImp.java
@@ -1,9 +1,11 @@
 package com.food.delivery.Service.ServiceImp;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.food.delivery.DataTransferObject.SetmealDto;
 import com.food.delivery.Entity.Setmeal;
 import com.food.delivery.Entity.SetmealDish;
+import com.food.delivery.Exception.CustomerizedException;
 import com.food.delivery.Mapper.SetmealMapper;
 import com.food.delivery.Service.SetmealDishService;
 import com.food.delivery.Service.SetmealService;
@@ -40,5 +42,40 @@ public class SetmealServiceImp extends ServiceImpl<SetmealMapper, Setmeal>
         .collect(Collectors.toList());
 
     setmealDishService.saveBatch(setmealDishes);
+  }
+
+  @Override
+  public void batchDeleteSetmeals(List<Long> ids) {
+
+    // check if status == 0 -> shall not delete
+    LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+    // count(*) from Setmeal s where s.id in (id1,id2...) and s.status = 1
+    queryWrapper.in(Setmeal::getId, ids);
+    queryWrapper.eq(Setmeal::getStatus, 1);
+    int count = this.count(queryWrapper);
+
+    if (count > 0) {
+      throw new CustomerizedException(
+          "At least one of the setMeal is enabled. Disable all before deletion");
+    }
+
+    // delete the setmeal from setmeal table
+    this.removeByIds(ids);
+    // delete setmealDish from setmealDish table
+    LambdaQueryWrapper<SetmealDish> queryWrapper2 = new LambdaQueryWrapper();
+    queryWrapper2.in(SetmealDish::getDishId, ids);
+    setmealDishService.remove(queryWrapper2);
+  }
+
+  @Override
+  public void toggleStatus(int newStatus, List<Long> ids) {
+
+    LambdaQueryWrapper<Setmeal> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+    lambdaQueryWrapper.in(Setmeal::getId, ids);
+
+    Setmeal setmeal = new Setmeal();
+    setmeal.setStatus(newStatus);
+
+    this.update(setmeal, lambdaQueryWrapper);
   }
 }
--- src/main/java/com/food/delivery/Service/SetmealService.java
+++ src/main/java/com/food/delivery/Service/SetmealService.java
@@ -3,7 +3,12 @@ package com.food.delivery.Service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.food.delivery.DataTransferObject.SetmealDto;
 import com.food.delivery.Entity.Setmeal;
+import java.util.List;
 
 public interface SetmealService extends IService<Setmeal> {
   public void saveSetmealWithDishes(SetmealDto setmealDto);
+
+  public void batchDeleteSetmeals(List<Long> ids);
+
+  public void toggleStatus(int newStatus, List<Long> ids);
 }
